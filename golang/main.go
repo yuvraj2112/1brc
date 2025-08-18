b69a321a4a0f41558979db5ae87c2ec4b064816b
yuvraj2112 <yuvraj2112@users.noreply.github.com>
2025-08-18 19:40:14 +0530

    Go added - Naive approach - 3:33

---
+++ golang/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)	
+	}
+}
+
+type CityInfo struct {
+    min float32
+    max float32
+		total float32
+		count int
+}
+
+type CityMap map[string]CityInfo
+
+var mapper = make(CityMap)
+
+func updateMap(city string, temp float32) {
+	value1, exists1 := mapper[city]
+	if exists1 {
+		value1.min = min(value1.min, temp)
+		value1.max = max(value1.max, temp)
+		value1.count += 1
+		value1.total += temp
+	} else {
+		mapper[city] = CityInfo{
+			min: temp,
+			max: temp,
+			total: temp,
+			count: 1,
+		}
+	}
+}
+
+func main() {
+	start := time.Now()
+	file, err := os.Open("../measurements.txt")
+	check(err)
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		city, temp, hasColon := strings.Cut(line,";")
+		if !hasColon {
+			continue
+		}
+
+		t, e := strconv.ParseFloat(temp, 32)
+		if e != nil {
+			log.Fatal(e)
+		}
+		updateMap(city, float32(t))
+	}
+	fmt.Println(mapper)
+	elapsed := time.Since(start) // Calculate elapsed time
+	fmt.Printf("Execution took %s\n", elapsed)
+}
