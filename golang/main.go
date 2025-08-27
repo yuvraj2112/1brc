ca25ec33c91c70963039010daedab7c9bd65bc15
yuvraj2112 <yuvraj2112@users.noreply.github.com>
2025-08-27 22:28:34 +0530

    19.4 seconds! - Goroutines

--- golang/main.go
+++ golang/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	// "bufio"
-	// "bytes"
+	"bytes"
 	"fmt"
 	// "io"
 	"log"
@@ -11,40 +11,28 @@ import (
 	// "strings"
 	"time"
 	"math"
+	"sync"
 	"sort"
+	"runtime"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)	
-	}
-}
-
 type CityInfo struct {
-    min int
-    max int
-		total int
-		count int
+	min int
+	max int
+	total int
+	count int
 }
 
 type CityMap map[string]CityInfo
 
-var mapper = make(CityMap)
+type FileChunkEdges struct {
+	start int
+	end int
+}
 
-func updateMap(city string, temp int) {
-	value1, exists1 := mapper[city]
-	if exists1 {
-		value1.min = min(value1.min, temp)
-		value1.max = max(value1.max, temp)
-		value1.count += 1
-		value1.total += temp
-	} else {
-		mapper[city] = CityInfo{
-			min: temp,
-			max: temp,
-			total: temp,
-			count: 1,
-		}
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)	
 	}
 }
 
@@ -110,12 +98,10 @@ func printCompiledResults(aggregations CityMap) {
 
 }
 
-func main() {
-	start := time.Now()
-	file, err := os.Open("../../measurements.txt")
-	check(err)
+func processValue(start int, end int, wg* sync.WaitGroup, outchan chan CityMap) {
+	defer wg.Done()
+	file, _ := os.Open("../../measurements.txt")
 	defer file.Close()
-
 	b := make([]byte, 8192)
 	nameMode := true
 	var temp [5]byte
@@ -123,11 +109,36 @@ func main() {
 	nameIdx := 0
 	tempIdx := 0
 
-	for {
-		n, _ := file.Read(b)
+	readFrom := start
+
+	var mapper = make(CityMap)
+
+	updateMap := func(city string, temp int) {
+		value1, exists1 := mapper[city]
+		if exists1 {
+			value1.min = min(value1.min, temp)
+			value1.max = max(value1.max, temp)
+			value1.count += 1
+			value1.total += temp
+		} else {
+			mapper[city] = CityInfo{
+				min: temp,
+				max: temp,
+				total: temp,
+				count: 1,
+			}
+		}
+	}
+
+	for ;readFrom < end; {
+		n, _ := file.ReadAt(b, int64(readFrom))
 		if n == 0 {
 			break
 		}
+		if (readFrom + n) > end {
+			n = end - readFrom
+		}
+		readFrom += n
 		for i := 0; i < n; i += 1 {
 			readByte := b[i]
 			switch readByte {
@@ -150,7 +161,89 @@ func main() {
 			}
 		}
 	}
-	printCompiledResults(mapper)
+	outchan <- mapper
+}
+
+func readFile(linesChan chan FileChunkEdges, wg* sync.WaitGroup) {
+	defer wg.Done()
+
+	file, _ := os.Open("../../measurements.txt")
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	check(err)
+
+	fileSize := fileInfo.Size()
+	cpuCount := runtime.NumCPU() + 2
+	each := int(float64(fileSize / int64(cpuCount)))
+	fmt.Println(fileSize, cpuCount)
+	
+	start, end := 0, each
+	buf := make([]byte, 107)
+	for i := 0; i < cpuCount; i += 1 {
+		file.ReadAt(buf, int64(end))
+		indexOfEnd := bytes.IndexByte(buf, 10)
+		if ((end + indexOfEnd) > int(fileSize)) {
+			end = (int(fileSize) - indexOfEnd)
+		}
+		linesChan <- FileChunkEdges{ start: start, end: end + indexOfEnd }
+		start = end + indexOfEnd + 1
+		end = start + each
+	}
+	close(linesChan)
+}
+
+func main() {
+	start := time.Now()
+	ch := make(chan FileChunkEdges, 14)
+	outch := make(chan CityMap, 14)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go readFile(ch, &wg)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for elem := range ch {
+			wg.Add(1)
+      go processValue(elem.start, elem.end, &wg, outch)
+    }
+	}()
+
+	var globalMap = make(CityMap) 
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		i := 0
+		for localMap := range outch {
+			i += 1
+			for city, info := range localMap {
+				if existing, ok := globalMap[city]; ok {
+					if info.min < existing.min {
+						existing.min = info.min
+					}
+					if info.max > existing.max {
+						existing.max = info.max
+					}
+					existing.total += info.total
+					existing.count += info.count
+					globalMap[city] = existing
+				} else {
+					globalMap[city] = info
+				}
+			}
+			if i == 14 {
+				close(outch)
+			}
+		}
+	}()
+
+	// go processValue(ch, &wg)
+	wg.Wait()
+
+	
+	printCompiledResults(globalMap)
 	elapsed := time.Since(start)
 	fmt.Printf("Execution took %s\n", elapsed)
 }
