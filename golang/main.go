339ced4464da01808e7698f7911c0ff6589d8ac7
yuvraj2112 <yuvraj2112@users.noreply.github.com>
2025-08-27 19:20:41 +0530

    Handle bytes directly + Ints 1:16 m

--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
+	// "bytes"
 	"fmt"
+	// "io"
 	"log"
 	"os"
-	"strings"
+	// "strconv"
+	// "strings"
 	"time"
-	"strconv"
+	"math"
+	"sort"
 )
 
 func check(err error) {
@@ -17,9 +21,9 @@ func check(err error) {
 }
 
 type CityInfo struct {
-    min float32
-    max float32
-		total float32
+    min int
+    max int
+		total int
 		count int
 }
 
@@ -27,7 +31,7 @@ type CityMap map[string]CityInfo
 
 var mapper = make(CityMap)
 
-func updateMap(city string, temp float32) {
+func updateMap(city string, temp int) {
 	value1, exists1 := mapper[city]
 	if exists1 {
 		value1.min = min(value1.min, temp)
@@ -44,27 +48,109 @@ func updateMap(city string, temp float32) {
 	}
 }
 
+func parseBufferToDigit(b byte) int {
+	return int(b - 0x30)
+}
+
+func tempParse(number *[5]byte, length int) int {
+	if number[0] == 0x2d { // '-'
+		if length == 5 {
+			return -(parseBufferToDigit(number[1])*100 +
+				parseBufferToDigit(number[2])*10 +
+				parseBufferToDigit(number[4]))
+		}
+		// length == 4
+		return -(parseBufferToDigit(number[1])*10 +
+			parseBufferToDigit(number[3]))
+	} else {
+		if length == 3 {
+			return parseBufferToDigit(number[0])*10 +
+				parseBufferToDigit(number[2])
+		}
+		// length == 4
+		return parseBufferToDigit(number[0])*100 +
+			parseBufferToDigit(number[1])*10 +
+			parseBufferToDigit(number[3])
+	}
+}
+
+func nameParse(nameBytes *[100]byte, idx int) string {
+	return string((*nameBytes)[:idx])
+}
+
+func round(num float64) float64 {
+	return math.Round(num*10) / 10.0
+}
+
+func printCompiledResults(aggregations CityMap) {
+	stations := make([]string, 0, len(aggregations))
+	for k := range aggregations {
+		stations = append(stations, k)
+	}
+	sort.Strings(stations)
+
+	result := "{"
+	for i, station := range stations {
+		data := aggregations[station]
+		avg := float64(data.total) / 10.0 / float64(data.count)
+		entry := fmt.Sprintf("%s=%f/%f/%f",
+			station,
+			round(float64(data.min)/10.0),
+			round(avg),
+			round(float64(data.max)/10.0),
+		)
+		if i > 0 {
+			result += ", "
+		}
+		result += entry
+	}
+	result += "}"
+
+	fmt.Println(result)
+
+}
+
 func main() {
 	start := time.Now()
-	file, err := os.Open("../measurements.txt")
+	file, err := os.Open("../../measurements.txt")
 	check(err)
 	defer file.Close()
-	
-	scanner := bufio.NewScanner(file)
-	check(scanner.Err())
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		city, temp, hasColon := strings.Cut(line,";")
-		if !hasColon {
-			continue
+	b := make([]byte, 8192)
+	nameMode := true
+	var temp [5]byte
+	var name [100]byte
+	nameIdx := 0
+	tempIdx := 0
+
+	for {
+		n, _ := file.Read(b)
+		if n == 0 {
+			break
 		}
-
-		t, e := strconv.ParseFloat(temp, 32)
-		check(e)
-		updateMap(city, float32(t))
+		for i := 0; i < n; i += 1 {
+			readByte := b[i]
+			switch readByte {
+				case 0x3b: // ;
+					nameMode = false
+				case 0x0a: // LE
+					// process items here
+					updateMap(nameParse(&name, nameIdx), tempParse(&temp, tempIdx))
+					nameIdx = 0
+					tempIdx = 0
+					nameMode = true
+				default:
+					if nameMode {
+						name[nameIdx] = readByte
+						nameIdx += 1
+					} else {
+						temp[tempIdx] = readByte
+						tempIdx += 1
+					}
+			}
+		}
 	}
-	fmt.Println(mapper)
-	elapsed := time.Since(start) // Calculate elapsed time
+	printCompiledResults(mapper)
+	elapsed := time.Since(start)
 	fmt.Printf("Execution took %s\n", elapsed)
 }
